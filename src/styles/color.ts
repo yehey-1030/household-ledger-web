af8e9040acc7b4a860a06efda0468021448f9ede
park ye jin <dyddyd0514@naver.com>
2023-12-03 17:54:27 +0900

    [Feat] basic style 파일 생성

---
+++ src/styles/color.ts
@@ -0,0 +1,21 @@
+export const WHITE = "#FFFFFF"
+export const BLACK = "#000000"
+export const GREY={
+  100:'#B3B3B3',
+  200:'#4E4E4E',
+  300:"#D9D9D9"
+}
+export const MAJOR_GREEN={
+  100:"#8ACCB8",
+  200:"#68AF9A",
+  300:"#E8F5F1",
+}
+
+const color={
+  WHITE,
+  BLACK,
+  GREY,
+  MAJOR_GREEN
+}
+
+export default color
---
+++ src/styles/font.ts
@@ -0,0 +1,22 @@
+export const fontSize = {
+  12: '1.2rem',
+  14: '1.4rem',
+  15: '1.5rem',
+  16: '1.6rem',
+  20: '2rem',
+  24: '2.4rem',
+};
+
+export const fontWeight = {
+  regular: 400,
+  medium: 500,
+  semibold: 600,
+  bold: 700,
+};
+
+const font = {
+  fontSize,
+  fontWeight,
+};
+
+export default font;
