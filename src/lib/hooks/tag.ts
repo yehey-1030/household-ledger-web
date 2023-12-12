410691089b86c9eea071a01ca63f52d0fb6c4a58
park ye jin <dyddyd0514@naver.com>
2023-12-12 16:15:31 +0900

    [Feat] get tag custom hook

---
+++ src/lib/hooks/tag.ts
@@ -0,0 +1,63 @@
+'use client';
+
+import { useQueries, useQuery } from '@tanstack/react-query';
+import { getChildTagList, getRootTagList } from '../api/tag';
+import { useEffect, useState } from 'react';
+import { TagType } from '@/types/tag';
+
+export const useRootTag = (typeID: number) => {
+  const { data: selectableTagList } = useQuery({
+    queryKey: ['rootTag', typeID],
+    queryFn: () => getRootTagList(typeID),
+  });
+
+  return { selectableTagList };
+};
+
+export const useChildTags = (parentID: number) => {
+  const { data: childTagList } = useQuery({
+    queryKey: ['childTag', parentID],
+    queryFn: () => getChildTagList(parentID),
+  });
+
+  return { childTagList };
+};
+
+export const useBasicTags = (typeID: number) => {
+  const { data: basicTags } = useQuery({
+    queryKey: ['rootTag', 6],
+    queryFn: () => getRootTagList(6),
+    enabled: typeID === 3,
+  });
+
+  return { basicTags };
+};
+
+export const useChildTagList = (tagList: number[]) => {
+  const [childTagList, setChildTagList] = useState<TagType[]>([]);
+
+  const queryResult = useQueries({
+    queries: tagList.map((tag) => {
+      return { queryKey: ['childTag', tag], queryFn: () => getChildTagList(tag) };
+    }),
+    combine: (results) => {
+      return {
+        data: results.map((result) => {
+          return result.data;
+        }),
+        pending: results.some((result) => result.isPending),
+      };
+    },
+  });
+
+  useEffect(() => {
+    setChildTagList([]);
+    queryResult.data.forEach((r) => {
+      if (r) {
+        setChildTagList((prev) => [...prev, ...r]);
+      }
+    });
+  }, [queryResult.data]);
+
+  return { childTagList };
+};
