4e105548ce93192d55779c7984d3a208050db105
park ye jin <dyddyd0514@naver.com>
2023-12-14 16:32:21 +0900

    [Add] Color type, new ledger background color

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -1,21 +1,32 @@
-export const WHITE = "#FFFFFF"
-export const BLACK = "#000000"
-export const GREY={
-  100:'#B3B3B3',
-  200:'#4E4E4E',
-  300:"#D9D9D9"
-}
-export const MAJOR_GREEN={
-  100:"#8ACCB8",
-  200:"#68AF9A",
-  300:"#E8F5F1",
-}
+type ColorType = {
+  [index: string]: string;
+};
 
-const color={
+export const WHITE = '#FFFFFF';
+export const BLACK = '#000000';
+export const GREY: ColorType = {
+  100: '#B3B3B3',
+  200: '#4E4E4E',
+  300: '#D9D9D9',
+};
+export const MAJOR_GREEN: ColorType = {
+  100: '#8ACCB8',
+  200: '#68AF9A',
+  300: '#E8F5F1',
+};
+export const LEDGER_BACKGROUND: ColorType = {
+  2: '#F3F1CC',
+  3: MAJOR_GREEN[300],
+  4: '#DDD9E6',
+  5: '#F5E1E1',
+};
+
+const color = {
   WHITE,
   BLACK,
   GREY,
-  MAJOR_GREEN
-}
+  MAJOR_GREEN,
+  LEDGER_BACKGROUND,
+};
 
-export default color
+export default color;
