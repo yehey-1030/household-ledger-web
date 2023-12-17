d48b55e9eb641abd079786b66555623345ba9e28
park ye jin <dyddyd0514@naver.com>
2023-12-17 18:39:13 +0900

    [Feat] Basic Layout component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
-'use client';
-
-import { Header } from '@/components/common';
+import { Header, Layout } from '@/components/common';
 import { LedgerList } from '@/components/home';
 
 import React, { Suspense } from 'react';
 
 export default function Home() {
   return (
-    <>
+    <Layout>
       <Header title="HOME" goback={false} canCreate />
       <Suspense fallback={<div>Loading...</div>}>
         <LedgerList />
       </Suspense>
-    </>
+    </Layout>
   );
 }
