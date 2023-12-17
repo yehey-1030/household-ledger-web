c0a607c40b29afc23d436b03072ea0e012d6e55a
park ye jin <dyddyd0514@naver.com>
2023-12-17 18:38:41 +0900

    [Feat] IconDescriptorType, add precedence

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Viewport, Metadata } from 'next';
 import StyledComponentsRegistry from '@/lib/utils/StyledSheetManager';
 import ReactQueryProvider from '@/lib/utils/ReactQueryProvider';
+import { CustomIconDescriptorType } from '@/types/custom';
 
 interface IRootLayoutProps {
   children: React.ReactNode;
@@ -13,14 +14,16 @@ export const viewport: Viewport = {
   userScalable: false,
 };
 
+const icon: CustomIconDescriptorType = {
+  rel: 'stylesheet',
+  url: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined',
+  precedence: 'default',
+};
+
 export const metadata: Metadata = {
   title: '언니의 가계부',
   icons: {
-    other: {
-      rel: 'stylesheet',
-      url: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined',
-      precedence: 'default',
-    },
+    other: icon,
   },
 };
 
