842126f4fae38cd1c0651a131444bb6b5a58ff83
park ye jin <dyddyd0514@naver.com>
2024-01-04 13:34:09 +0900

    [Feat] login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from '@/components/auth';
+import { Layout } from '@/components/common';
+
+export default function LoginPage() {
+  return (
+    <Layout>
+      <LoginForm />
+    </Layout>
+  );
+}
