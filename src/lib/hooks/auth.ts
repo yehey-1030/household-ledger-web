870928e1969b27388c9d736ae6049571ae51f88f
park ye jin <dyddyd0514@naver.com>
2024-01-04 13:36:04 +0900

    [Add] login state validate hook

--- src/lib/hooks/auth.ts
+++ src/lib/hooks/auth.ts
@@ -1,16 +1,42 @@
-import { useSetRecoilState } from 'recoil';
-import { loginState } from '../store';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { loginState, loginStateSelector } from '../store';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export const useLogin = () => {
   const setIsLoggedIn = useSetRecoilState(loginState);
+  const router = useRouter();
 
   const login = (inputPW: string) => {
     if (inputPW === process.env.NEXT_PUBLIC_LOGIN_PASSWORD) {
       setIsLoggedIn({ isLoggedIn: true });
+      router.push('/');
     } else {
       setIsLoggedIn({ isLoggedIn: false });
     }
   };
 
   return { login };
+};
+
+export const useIsLoggedIn = () => {
+  const isLoggedIn = useRecoilValue(loginStateSelector);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const router = useRouter();
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    router.push('/login');
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      openModal();
+    }
+  }, []);
+
+  return { isLoggedIn, closeModal, isModalOpen };
 };
--- src/lib/hooks/index.ts
+++ src/lib/hooks/index.ts
@@ -1,3 +1,6 @@
+'use client';
+
 export * from './ledger';
 export * from './tag';
 export * from './statistics';
+export * from './auth';
